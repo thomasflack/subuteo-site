4f210c1fb45734db433dde80d60b13240f1abe9b
thomasflack <thomas@geekeye.co.uk>
2015-12-08 18:52:57 +0000

    1516516

--- www/info.php
+++ www/info.php
@@ -1,4 +1,13 @@
 <?php
+include ($_SERVER['DOCUMENT_ROOT'] . '/database/testDbFactory.php');
 
 $objDbFactory = new testDbFactory();
 $objDb = $objDbFactory->create();
+
+$strQuery = 'SELECT * FROM test.testtable';
+
+$result = $objDb->query($strQuery);
+
+$row = mysqli_fetch_assoc($result);
+
+print_r($row);
